f94d37c552f8d3da5c07df31ca0b5a57162e462a
Antonio <54863109+ilusinovac@users.noreply.github.com>
2023-11-13 00:22:41 +0100

    Create zadaca3.cpp

---
+++ Za vezbanje kolokvium/zadaca3.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+int main ()
+{
+
+  int n;
+  cin >> n;
+  int temp, digit, interesen, obraten = 0, brojac = 0, najgolem = 0;
+  for (int i = 1; i < n; i++)
+    {
+      temp = i;
+      obraten = 0;
+      brojac = 0;
+      while (temp != 0)
+	{
+	  digit = temp % 10;
+	  brojac++;
+	  obraten += digit;
+	  temp /= 10;
+	  if (temp == 0)
+	    {
+	      break;
+	    }
+
+	  obraten = obraten * 10;
+	}
+      if (obraten % brojac == 0)
+	{
+	  interesen = i;
+	  if (interesen > najgolem)
+	    {
+	      najgolem = interesen;
+	    }
+	}
+      
+    }
+  if (n < 10)
+    {
+      cout << "Brojot ne e validen";
+    }
+  else
+    {
+      cout << najgolem;
+    }
+
+
+  return 0;
+}
