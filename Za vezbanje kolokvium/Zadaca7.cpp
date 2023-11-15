350686439ffbee515abb6da956882e6c4dbcbac0
Antonio <54863109+ilusinovac@users.noreply.github.com>
2023-11-15 01:49:41 +0100

    Update Zadaca7.cpp
    Ispravka

--- Za vezbanje kolokvium/Zadaca7.cpp
+++ Za vezbanje kolokvium/Zadaca7.cpp
@@ -1,55 +1,35 @@
 /*
 
-DAVA POGRESEN OUTPUT PRIMER:
-
-%%%%%%%%%%%                
-%.........%
-%.........%
-%.........%
-%.........%
-%.........%
-%.........%
-%.........%
-%.........%
-%.........%
-%%%%%%%%%%%
-
-NAMESTO : 
-
-%@@@@@@@@@%
-%.........%
-%.........%
-%.........%
-%.........%
-%.........%
-%.........%
-%.........%
-%.........%
-%.........%
-%@@@@@@@@@%
-
 */
 
 #include <iostream>
 
 using namespace std;
 
-int main() {
-    int m;
-    cin >> m;
-
-    for (int i = 1; i <= m; ++i) {
-        for (int j = 1; j <= m; ++j) {
-            if (i == 1 || i == m || j == 1 || j == m) {
-                cout << "%";
-            } else if (i > 1 && i < m && j > 1 && j < m) {
-                cout << ".";
-            } else {
-                cout << "@";
-            }
-        }
-        cout << endl;
+int main ()
+{
+  int m;
+  cin >> m;
+  for (int i = 1; i <= m; ++i)
+    {
+      for (int j = 1; j <= m; ++j)
+	{
+	  if (j == 1 || j == m)
+	    {
+	      cout << "%";
+	    }
+	  else if (i == 1 || i == m)
+	    {
+	      cout << "@";
+	    }
+	  else
+	    {
+	      cout << ".";
+	    }
+	}
+      cout << endl;
     }
 
-    return 0;
+  return 0;
 }
+
